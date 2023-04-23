e8b0402b53564e25d71b73f02b7d99845210c337
Johnny Watts <warhamr1488@gmail.com>
2023-04-23 03:21:05 -0400

    Update kmap.py

--- kmap.py
+++ kmap.py
@@ -16,22 +16,34 @@ FGC='\033[48;5;237m'
 NC='\033[0m'
 LIGHT_CYAN='\033[1;96m'
 
-print(f" \n{FGC}{GREEN} KMap v 0.1 {NC}{LIGHT_MAGENTA}\n\n KMap is a user friendly tool for running various types of nmap scans allowing user to set scan parameters by making simple choices.\n")
-print(f"{FGC}{LIGHT_CYAN} 🕵🔎 Courtesy of KaotickJ 👽 {NC}\n")
+
+print(f" \n")
+print(f" {RED} ██ ▄█▀ ███▄ ▄███▓ ▄▄▄       ██▓███  ")
+print(f" {RED} ██▄█▒ ▓██▒▀█▀ ██▒▒████▄    ▓██░  ██▒")
+print(f" {RED}▓███▄░ ▓██    ▓██░▒██  ▀█▄  ▓██░ ██▓▒")
+print(f" {RED}▓██ █▄ ▒██    ▒██ ░██▄▄▄▄██ ▒██▄█▓▒ ▒")
+print(f" {RED}▒██▒ █▄▒██▒   ░██▒ ▓█   ▓██▒▒██▒ ░  ░")
+print(f" {RED}▒ ▒▒ ▓▒░ ▒░   ░  ░ ▒▒   ▓▒█░▒▓▒░ ░  ░")
+print(f" {RED}░ ░▒ ▒░░  ░      ░  ▒   ▒▒ ░░▒ ░     ")
+print(f" {RED}░ ░░ ░ ░      ░     ░   ▒   ░░       ")
+print(f" {RED}░  ░          ░         ░  ░         ")
+print(f"	{FGC}{LIGHT_CYAN} 🕵🔎 Courtesy of KaotickJ 👽 {NC}\n")
+
+print(f" \n KMap v 0.1 {NC}{LIGHT_MAGENTA}\n\n KMap is a user friendly tool for running various types of nmap scans allowing user to set scan parameters by making simple choices.\n")
 while True:
-    ip_address = input(f"{BLUE}Enter the IP address or hostname to scan: {DG}")
+    ip_address = input(f"{FGC}{GREEN} Enter the IP address or hostname to scan:{NC} {DG}")
 
     # Validate input as IP address or hostname
     if re.match('^([0-9]{1,3}\.){3}[0-9]{1,3}$', ip_address) or re.match('^([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.[a-zA-Z]{2,}$', ip_address):
         break
     else:
-        print(f"Input, \"{ip_address}\" is not a valid IP address or hostname")
+        print(f"{RED} Input, \"{ip_address}\" is not a valid IP address or hostname{NC}")
         continue
 
 while True:
     # Prompt user to choose scan type
-    print(f"{BLUE}Please choose a scan type from the list below:\n{YELLOW}1. TCP SYN Scan (-sS)\n2. TCP Connect Scan (-sT)\n3. UDP Scan (-sU)\n4. TCP + UDP + Service Detection (-sS -sU -sV)\n")
-    scan_type_choice = input(f"{BLUE}Enter a number: {DG}")
+    print(f"\n{BLUE} Please choose a scan type from the list below:\n{YELLOW} 1. TCP SYN Scan (-sS)\n 2. TCP Connect Scan (-sT)\n 3. UDP Scan (-sU)\n 4. TCP + UDP + Service Detection (-sS -sU -sV)\n")
+    scan_type_choice = input(f"{FGC}{GREEN} Enter a number:{NC} {DG}")
 
     if scan_type_choice == '1':
         scan_type = "-sS"
@@ -46,13 +58,13 @@ while True:
         scan_type = "-sS -sU -sV"
         break
     else:
-        print(f"{RED}Invalid scan type choice: \"{scan_type_choice}\"")
+        print(f"{RED} Invalid scan type choice: \"{scan_type_choice}\"")
         continue
 
 while True:
     # Prompt user to choose timing option
-    print(f"\n{BLUE}Please choose a timing option from the list below:\n{YELLOW}1. T0 (Paranoid)\n2. T1 (Sneaky)\n3. T2 (Polite)\n4. T3 (Normal)\n5. T4 (Aggressive)\n6. T5 (Insane)\n")
-    timing_choice = input(f"{BLUE}Enter a number: {DG}")
+    print(f"\n{BLUE} Please choose a timing option from the list below:\n{YELLOW} 1. T0 (Paranoid)\n 2. T1 (Sneaky)\n 3. T2 (Polite)\n 4. T3 (Normal)\n 5. T4 (Aggressive)\n 6. T5 (Insane)\n")
+    timing_choice = input(f"{FGC}{GREEN}Enter a number:{NC} {DG}")
 
     if timing_choice == '1':
         timing_option = "-T0"
@@ -73,13 +85,13 @@ while True:
         timing_option = "-T5"
         break
     else:
-        print(f"{RED}Invalid timing option choice")
+        print(f"{RED} Invalid timing option choice")
         continue
 
 while True:
     # Prompt user to choose script option
-    print(f"\n{BLUE}Please choose a script option from the list below:\n{YELLOW}1. default (-sC)\n2. vuln\n3. auth\n4. intrusive\n5. safe\n6. all\n7. none\n")
-    script_choice = input(f"{BLUE}Enter a number: {DG}")
+    print(f"\n{BLUE} Please choose a script option from the list below:\n{YELLOW} 1. default (-sC)\n 2. vuln\n 3. auth\n 4. intrusive\n 5. safe\n 6. all\n 7. none\n")
+    script_choice = input(f"{FGC}{GREEN} Enter a number:{NC} {DG}")
 
     if script_choice == '1':
         script_option = "-sC"
@@ -103,12 +115,12 @@ while True:
         script_option = ""
         break
     else:
-        print(f"\n{RED}Invalid choice. Please choose a valid script option.{DG}")
+        print(f"\n{RED} Invalid choice. Please choose a valid script option.{DG}")
 
 while True:
     # Prompt user to choose port scan option
-    print(f"\n{BLUE}Please choose a ports to scan option from the list below:\n{YELLOW}1. Top 20\n2. Top 100\n3. Top 1000\n4. All Ports\n5. Custom Port Range\n")
-    ports_choice = input(f"{BLUE}Enter a number: {DG}")
+    print(f"\n{BLUE} Please choose a ports to scan option from the list below:\n{YELLOW} 1. Top 20\n 2. Top 100\n 3. Top 1000\n 4. All Ports\n 5. Custom Port Range\n")
+    ports_choice = input(f"{FGC}{GREEN} Enter a number:{NC} {DG}")
 
     if ports_choice == '1':
         ports_option = "--top-ports 20"
@@ -123,7 +135,7 @@ while True:
         ports_option = "-p-"
         break
     elif ports_choice == '5':
-        ports_range = input(f"{BLUE}Enter a port range (i.e, 1-1024): {DG}")
+        ports_range = input(f"{FGC}{GREEN} Enter a port range (i.e, 1-1024):{NC} {DG}")
         ports_option = f"-p {ports_range}"
         break
     else:
@@ -131,8 +143,8 @@ while True:
         
 while True:
     # Prompt user to choose aggessive scan options
-    print(f"\n{BLUE}Please choose aggressive scan options from the list below:\n(Note that these scan types are very slow, and may require root permission)\n{YELLOW}1. Aggressive service detection (-A)\n2. OS Detection (-O)\n3. Aggressive Service Detection and OS Detection (-A -O)\n4. None\n")
-    aggressive_choice = input(f"{BLUE}Enter a number: {DG}")
+    print(f"\n{BLUE} Please choose aggressive scan options from the list below:\n (Note that these scan types are very slow, and may require root permission)\n{YELLOW} 1. Aggressive service detection (-A)\n 2. OS Detection (-O)\n 3. Aggressive Service Detection and OS Detection (-A -O)\n 4. None\n")
+    aggressive_choice = input(f"{FGC}{GREEN} Enter a number:{NC} {DG}")
 
     if aggressive_choice == '1':
         aggressive_option = "-A"
@@ -147,13 +159,14 @@ while True:
         aggressive_option = ""
         break
     else:
-        print(f"\n{RED}Invalid choice. Please choose a valid aggressive scan option.{DG}")
+        print(f"\n{RED} Invalid choice. Please choose a valid aggressive scan option.{DG}")
 
 # Construct Nmap command
 nmap_command = f"sudo nmap -vv {aggressive_option} {scan_type} {timing_option} {script_option} {ip_address} {ports_option} -oA {ip_address}"
 
 # Run Nmap command and print output
-print(f"{BLUE}Running Nmap scan command: {DG}{nmap_command}")
+print(f"\n{GREEN} Running Nmap scan command: {DG}{nmap_command}")
 scan_output = os.popen(nmap_command).read()
-print(f"\n{BLUE}Nmap scan output:\n{DG}{scan_output}")
+print(f"\n{BLUE} Nmap scan output:\n{DG}{scan_output}")
+print(f"\n{GREEN} Nmap scan complete. \n{DG} Scan results saved to:\n {ip_address}.xml, {ip_address}.gnmap,and {ip_address}.nmap")
         
