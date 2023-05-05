0e89fc8b2896404e9643613bb66cee629cf3c65c
Johnny Watts <warhamr1488@gmail.com>
2023-05-05 18:32:34 -0400

    Update kmap.py
    replaces the command line interface with a Graphical User Interface. Note that this is only tested in linux

--- kmap.py
+++ kmap.py
@@ -1,192 +1,159 @@
 #!/usr/bin/env python3
-# Script: KMap v 0.1
+# Script: KMap v 0.6
 # Author: kaotickj
 # Website: kdgwebsolutions.com
 
 import re
 import os
-
-BLUE = '\033[1;34m'
-DG = '\033[0m'
-YELLOW = '\033[1;33m'
-RED = '\033[1;31m'
-LIGHT_MAGENTA='\033[1;95m'
-GREEN='\033[1;32m'
-FGC='\033[48;5;237m'
-NC='\033[0m'
-LIGHT_CYAN='\033[1;96m'
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+from tkinter import BooleanVar
 
 
-print(f" \n")
-print(f" {RED} ██ ▄█▀ ███▄ ▄███▓ ▄▄▄       ██▓███  ")
-print(f" {RED} ██▄█▒ ▓██▒▀█▀ ██▒▒████▄    ▓██░  ██▒")
-print(f" {RED}▓███▄░ ▓██    ▓██░▒██  ▀█▄  ▓██░ ██▓▒")
-print(f" {RED}▓██ █▄ ▒██    ▒██ ░██▄▄▄▄██ ▒██▄█▓▒ ▒")
-print(f" {RED}▒██▒ █▄▒██▒   ░██▒ ▓█   ▓██▒▒██▒ ░  ░")
-print(f" {RED}▒ ▒▒ ▓▒░ ▒░   ░  ░ ▒▒   ▓▒█░▒▓▒░ ░  ░")
-print(f" {RED}░ ░▒ ▒░░  ░      ░  ▒   ▒▒ ░░▒ ░     ")
-print(f" {RED}░ ░░ ░ ░      ░     ░   ▒   ░░       ")
-print(f" {RED}░  ░          ░         ░  ░         ")
-print(f"	{FGC}{LIGHT_CYAN} 🕵🔎 Courtesy of KaotickJ 👽 {NC}\n")
-
-print(f" \n KMap v 0.1 {NC}{LIGHT_MAGENTA}\n\n KMap is a user friendly tool for running various types of nmap scans allowing user to set scan parameters by making simple choices.\n")
-print(f" \n {NC}{LIGHT_CYAN}\n\n Before running the scan, you will need to set some options to construct the nmap command-line arguments:\n")
-
-while True:
-    ip_address = input(f" {FGC}{GREEN} Enter the IP address or hostname to scan:{NC} {DG}")
-
+def validate_ip_address(ip_address):
     # Validate input as IP address or hostname
-    if re.match('^([0-9]{1,3}\.){3}[0-9]{1,3}$', ip_address) or re.match('^([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.[a-zA-Z]{2,}$', ip_address):
-        break
+    if re.match('^([0-9]{1,3}\.){3}[0-9]{1,3}$', ip_address) or re.match(
+            '^([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.[a-zA-Z]{2,}$', ip_address):
+        return True
     else:
-        print(f"{RED} Input, \"{ip_address}\" is not a valid IP address or hostname{NC}")
-        continue
+        return False
 
-while True:
-    # Prompt user to choose verbosity level
-    print(f"\n{BLUE} Please choose a verbosity option from the list below:\n{YELLOW} 1. Normal Output\n 2. Verbose Output (-v)\n 3. Very Verbose Output (-vv)\n")
-    verbosity_choice = input(f" {FGC}{GREEN} Enter a number:{NC} {DG}")
 
-    if verbosity_choice == '1':
-        verbosity = ''
-        break
-    elif verbosity_choice == '2':
-        verbosity = "-v"
-        break
-    elif verbosity_choice == '3':
-        verbosity = "-vv"
-        break
-    else:
-        print(f"{RED} Invalid verbosity option: \"{verbosity_choice}\"")
-        continue
+def start_scan():
+    ip_address = ip_address_entry.get()
+    verbosity = verbosity_choice.get()
+    scan_type = scan_type_choice.get()
+    timing_option = timing_option_choice.get()
+    nmap_script_option = nmap_script_options_choice.get()
+    aggressive_scan_options = "-A -O" if is_aggressive_scan.get() else ""
 
-while True:
-    # Prompt user to choose scan type
-    print(f"\n{BLUE} Please choose a scan type from the list below:\n{YELLOW} 1. TCP SYN Scan (-sS)\n 2. TCP Connect Scan (-sT)\n 3. UDP Scan (-sU)\n 4. TCP + UDP + Service Detection (-sS -sU -sV)\n")
-    scan_type_choice = input(f" {FGC}{GREEN} Enter a number:{NC} {DG}")
+    if not validate_ip_address(ip_address):
+        messagebox.showerror("Error", f"Input, \"{ip_address}\" is not a valid IP address or hostname")
+        return
 
-    if scan_type_choice == '1':
-        scan_type = "-sS"
-        break
-    elif scan_type_choice == '2':
-        scan_type = "-sT"
-        break
-    elif scan_type_choice == '3':
-        scan_type = "-sU"
-        break
-    elif scan_type_choice == '4':
-        scan_type = "-sS -sU -sV"
-        break
-    else:
-        print(f"{RED} Invalid scan type choice: \"{scan_type_choice}\"")
-        continue
+    command = f"sudo nmap {verbosity} {scan_type} {timing_option} {nmap_script_option} {aggressive_scan_options} {ip_address} -oA kmapscan_results"
+    print(f"{command}")
 
-while True:
-    # Prompt user to choose timing option
-    print(f"\n{BLUE} Please choose a timing option from the list below:\n{YELLOW} 1. T0 (Paranoid)\n 2. T1 (Sneaky)\n 3. T2 (Polite)\n 4. T3 (Normal)\n 5. T4 (Aggressive)\n 6. T5 (Insane)\n")
-    timing_choice = input(f" {FGC}{GREEN}Enter a number:{NC} {DG}")
+    os.system(command)
+    messagebox.showinfo("Scan Complete", "Scan has completed. Output saved to kmapscan_results.{gnmap}{nmap}{xml}")
 
-    if timing_choice == '1':
-        timing_option = "-T0"
-        break
-    elif timing_choice == '2':
-        timing_option = "-T1"
-        break
-    elif timing_choice == '3':
-        timing_option = "-T2"
-        break
-    elif timing_choice == '4':
-        timing_option = "-T3"
-        break
-    elif timing_choice == '5':
-        timing_option = "-T4"
-        break
-    elif timing_choice == '6':
-        timing_option = "-T5"
-        break
-    else:
-        print(f"{RED} Invalid timing option choice")
-        continue
 
-while True:
-    # Prompt user to choose script option
-    print(f"\n{BLUE} Please choose a script option from the list below:\n (Note that some of these options are very slow, and may require root permission)\n{YELLOW} 1. default (-sC)\n 2. vuln\n 3. auth\n 4. intrusive\n 5. safe\n 6. all\n 7. none\n")
-    script_choice = input(f" {FGC}{GREEN} Enter a number:{NC} {DG}")
+root = tk.Tk()
+root.title("KMap v 0.6")
+logo_file = "alien.png"
+if os.path.exists(logo_file):
+    logo = PhotoImage(file=logo_file)
+    root.wm_iconphoto(True, logo)
 
-    if script_choice == '1':
-        script_option = "-sC"
-        break
-    elif script_choice == '2':
-        script_option = "--script vuln"
-        break
-    elif script_choice == '3':
-        script_option = "--script auth"
-        break
-    elif script_choice == '4':
-        script_option = "--script intrusive"
-        break
-    elif script_choice == '5':
-        script_option = "--script safe"
-        break
-    elif script_choice == '6':
-        script_option = "--script all"
-        break
-    elif script_choice == '7':
-        script_option = ""
-        break
-    else:
-        print(f"\n{RED} Invalid choice. Please choose a valid script option.{DG}")
+# Set up the options frame
+options_frame = ttk.LabelFrame(root, text="Scan Options")
+options_frame.grid(column=0, row=1, columnspan=2, padx=5, pady=5)
 
-while True:
-    # Prompt user to choose port scan option
-    print(f"\n{BLUE} Please choose a ports to scan option from the list below:\n{YELLOW} 1. Top 20\n 2. Top 100\n 3. Top 1000\n 4. All Ports\n 5. Custom Port Range\n")
-    ports_choice = input(f" {FGC}{GREEN} Enter a number:{NC} {DG}")
+# Set up the IP address entry field
+ip_address_label = ttk.Label(options_frame, text="IP address or hostname to scan:")
+ip_address_label.grid(column=0, row=0, padx=5, pady=5)
 
-    if ports_choice == '1':
-        ports_option = "--top-ports 20"
-        break
-    elif ports_choice == '2':
-        ports_option = "--top-ports 100"
-        break
-    elif ports_choice == '3':
-        ports_option = "--top-ports 1000"
-        break
-    elif ports_choice == '4':
-        ports_option = "-p-"
-        break
-    elif ports_choice == '5':
-        ports_range = input(f" {FGC}{GREEN} Enter a port range (i.e, 1-1024):{NC} {DG}")
-        ports_option = f"-p {ports_range}"
-        break
-    else:
-        print(f"\n{RED}Invalid choice. Please choose a valid ports option.{DG}")
-        
-while True:
-    # Prompt user to choose aggessive scan options
-    print(f"\n{BLUE} Please choose aggressive scan options from the list below:\n (Note that these scan types are very slow, and may require root permission)\n{YELLOW} 1. Aggressive service detection (-A)\n 2. OS Detection (-O)\n 3. Aggressive Service Detection and OS Detection (-A -O)\n 4. None\n")
-    aggressive_choice = input(f" {FGC}{GREEN} Enter a number:{NC} {DG}")
+ip_address_entry = ttk.Entry(options_frame)
+ip_address_entry.grid(column=1, row=0, padx=5, pady=5)
 
-    if aggressive_choice == '1':
-        aggressive_option = "-A"
-        break
-    elif aggressive_choice == '2':
-        aggressive_option = "-O"
-        break
-    elif aggressive_choice == '3':
-        aggressive_option = "-A -O"
-        break
-    elif aggressive_choice == '4':
-        aggressive_option = ""
-        break
-    else:
-        print(f"\n{RED} Invalid choice. Please choose a valid aggressive scan option.{DG}")
+# Set up the verbosity options
+verbosity_label = ttk.Label(options_frame, text="Choose a verbosity option:")
+verbosity_label.grid(column=0, row=1, padx=5, pady=5)
+verbosity_choice = tk.StringVar(value="-v")
+verbosity_options = [
+    {"text": "Normal Output", "value": ""},
+    {"text": "Verbose Output", "value": "-v"},
+    {"text": "Very Verbose Output", "value": "-vv"}
+]
 
-# Construct Nmap command
-nmap_command = f"sudo nmap {verbosity} {aggressive_option} {scan_type} {timing_option} {script_option} {ip_address} {ports_option} -oA {ip_address}"
+verbosity_combobox = ttk.Combobox(options_frame, textvariable=verbosity_choice,
+                                  values=[option["value"] for option in verbosity_options], state="readonly")
+verbosity_combobox.grid(column=1, row=1, padx=5, pady=5)
 
-# Run Nmap command and print output
-print(f"\n{GREEN} Running Nmap scan command: {DG}{nmap_command}")
-scan_output = os.popen(nmap_command).read()
-print(f"\n{GREEN} Nmap scan output:\n{DG}{scan_output}")
-print(f"\n{GREEN} Nmap scan complete. \n{LIGHT_CYAN} Scan results saved to:\n    {YELLOW}{ip_address}.xml\n    {ip_address}.gnmap\n    {ip_address}.nmap")
-        
+# Set up the scan type radio buttons
+scan_type_label = ttk.Label(options_frame, text="Choose a scan type:")
+scan_type_label.grid(column=0, row=2, padx=5, pady=5)
+
+scan_type_choice = tk.StringVar(value="-sS")
+
+scan_type_options = [
+    {"text": "TCP SYN scan (-sS)", "value": "-sS"},
+    {"text": "TCP connect scan (-sT)", "value": "-sT"},
+    {"text": "UDP scan (-sU)", "value": "-sU"},
+    {"text": "TCP NULL scan (-sN)", "value": "-sN"},
+    {"text": "TCP FIN scan (-sF)", "value": "-sF"},
+    {"text": "TCP Xmas scan (-sX)", "value": "-sX"}
+]
+
+# Create a frame to group the scan type options
+scan_type_frame = ttk.Frame(options_frame)
+scan_type_frame.grid(column=1, row=2, padx=5, pady=5)
+
+# Add a label and radiobutton for each scan type option
+for i, option in enumerate(scan_type_options):
+    ttk.Radiobutton(scan_type_frame, text=option["text"], variable=scan_type_choice, value=option["value"]).grid(
+        column=0, row=i, sticky="W", padx=5, pady=2)
+
+# Set up the Nmap script options Combobox
+nmap_script_options_label = ttk.Label(options_frame, text="Choose Nmap script options:")
+nmap_script_options_label.grid(column=0, row=3, padx=5, pady=5)
+
+nmap_script_options_choice = tk.StringVar(value="")  # default value is empty
+nmap_script_options = [
+    {"text": "No scripts", "value": ""},
+    {"text": "Authentication scripts (auth)", "value": "--script auth"},
+    {"text": "Default scripts (default)", "value": "--script default"},
+    {"text": "Exploit detection (exploit)", "value": "--script exploit"},
+    {"text": "Vulnerability detection (vuln)", "value": "--script vuln"},
+    {"text": "All scripts (all)", "value": "--script all"}
+]
+
+# Create a frame to group the script options
+nmap_script_options_frame = ttk.Frame(options_frame)
+nmap_script_options_frame.grid(column=1, row=3, padx=5, pady=5)
+
+# Add a label and radiobutton for each script option
+for i, option in enumerate(nmap_script_options):
+    ttk.Radiobutton(nmap_script_options_frame, text=option["text"], variable=nmap_script_options_choice,
+                    value=option["value"]).grid(column=0, row=i, sticky="W", padx=5, pady=2)
+
+# Set up the timing options radio buttons
+timing_option_label = ttk.Label(options_frame, text="Choose a timing option:")
+timing_option_label.grid(column=0, row=4, padx=5, pady=5)
+
+timing_option_choice = tk.StringVar(value="-T3")
+
+timing_option_options = [
+    {"text": "Paranoid timing (-T0)", "value": "-T0"},
+    {"text": "Sneaky timing (-T1)", "value": "-T1"},
+    {"text": "Polite timing (-T2)", "value": "-T2"},
+    {"text": "Default (normal) timing (-T3)", "value": "-T3"},
+    {"text": "Aggressive timing (-T4)", "value": "-T4"},
+    {"text": "Insane timing (-T5)", "value": "-T5"}
+]
+
+# Create a frame to group the timing options
+timing_option_frame = ttk.Frame(options_frame)
+timing_option_frame.grid(column=1, row=4, padx=5, pady=5)
+
+# Add a label and radiobutton for each timing option
+for i, option in enumerate(timing_option_options):
+    ttk.Radiobutton(timing_option_frame, text=option["text"], variable=timing_option_choice,
+                    value=option["value"]).grid(column=0, row=i, sticky="W", padx=5, pady=2)
+
+# Set up the Aggressive scan options Checkbutton
+# aggressive_scan_options_label = ttk.Label(root, text="Aggressive scan option:")
+# aggressive_scan_options_label.grid(column=0, row=2, padx=5, pady=5)
+
+is_aggressive_scan = tk.BooleanVar()
+aggressive_scan_options_checkbutton = ttk.Checkbutton(root,
+                                                      text="Enable aggressive service and OS detection options (-A -O)",
+                                                      variable=is_aggressive_scan)
+aggressive_scan_options_checkbutton.grid(column=0, row=2, padx=5, pady=5)
+
+# Add a button to start the scan
+start_button = ttk.Button(root, text="Start Scan", command=start_scan)
+start_button.grid(column=0, row=3, columnspan=2, padx=5, pady=5)
+root.mainloop()
